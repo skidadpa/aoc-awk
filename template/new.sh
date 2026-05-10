61310362b65dcf1290a42f364584cc922192fbfa
skidadpa <gsw@wfam.info>
2026-05-09 20:28:39 -0400

    update new.sh

--- template/new.sh
+++ template/new.sh
@@ -12,5 +12,12 @@ then
     echo "ERROR: $1 already exists" >&2
     exit 2
 fi
+num=${1#day}
+pref=${1%[0-2][0-9]}
+if [[ ("$pref" != "day") || ("$pref$num" != "$1") || ("$num" == "") ]]
+then
+    echo "ERROR: directory must be in form dayNN, where NN is a number from 00-29" >&2
+    exit 1
+fi
 cp -r ../template/day00 $1
 make -C $1 input.txt
